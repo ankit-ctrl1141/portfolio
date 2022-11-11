a596a9a3283b4ce48c40cb5f9e1f1932df976de0
Ankit Kumar Verma <ankit.official025@gmail.com>
2022-11-11 19:17:03 +0530

    fixed hamburger icon and menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,19 +4,19 @@ export default function Menu(props) {
   return (
     <div className={`menu ${props.menuOpen && "active"}`}>
         <ul>
-            <li>
+            <li onClick={props.handleClickHamburger}>
                 <a href="#intro">Home</a>
             </li>
-            <li>
+            <li onClick={props.handleClickHamburger}>
                 <a href="#portfolio">Portfolio</a>
             </li>
-            <li>
+            <li onClick={props.handleClickHamburger}>
                 <a href="#works">Works</a>
             </li>
-            <li>
+            <li onClick={props.handleClickHamburger}>
                 <a href="#testimonials">Testimonials</a>
             </li>
-            <li>
+            <li onClick={props.handleClickHamburger}>
                 <a href="#contact">Contact</a>
             </li>
             
