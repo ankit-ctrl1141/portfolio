8e28158a8d385cca3f310021a939120d12ecf40e
Ankit Kumar Verma <ankit.official025@gmail.com>
2022-11-11 13:06:20 +0530

    initailising project using create-react-app

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'  // We don't need to import this after React 17
+import './testimonials.scss'
+export default function Testimonials() {
+  return (
+    <div className='testimonials'>
+      
+    </div>
+  )
+}
